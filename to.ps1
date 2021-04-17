2ddc1e80169e835787f964dac25436a7c4c38997
Ploiu <curtislanglitz+github@gmail.com>
2021-04-17 17:57:54 -0400

    to.ps1

---
+++ to.ps1
@@ -0,0 +1,28 @@
+<#
+    .SYNOPSIS
+    travels up the directory tree until the passed folder is found, and then enters that folder
+
+    .DESCRIPTION
+    starting with the current directory, searches up the directory tree for the folder 
+    with the passed name. If it is found, enter into it. If it is not found, (we have hit the root drive),
+    we return to the original directory this script was called from
+#>
+param(
+    [string]$targetDir
+)
+# keep track of the original directory to return to
+$origDir = $pwd;
+# get the root directory in order to check if we've made it to the top
+$rootDir = [System.IO.Path]::GetPathRoot($pwd);
+while(!(Test-Path -PathType Container -Path "$targetDir")) {
+    # if we're at the root dir, tell the user the path could not be found and return
+    if($rootDir -eq $pwd) {
+        Write-Host -ForegroundColor red 'Directory not found. Returning';
+        Set-Location $origDir;
+        return;
+        break;
+    } else {
+        Set-Location ..;
+    }
+}
+Set-Location $targetDir
